cb3ea90e3502e0e635675f90fc201fc2a1b81e36
Rigelyon <pandudwi120@gmail.com>
2025-12-04 18:20:21 +0700

    Fix unable to locate file in Vite manifest

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.tsx', 'resources/js/landing.js'],
+            input: ['resources/js/app.tsx', 'resources/js/landing.js', 'resources/css/landing.css'],
             refresh: true,
         }),
         react(),
